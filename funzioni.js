e2719971cff0c67db08d092ba8a1ad769b2b7bef
arena <oscarbuselli3@gmail.com>
2025-03-16 16:19:51 +0100

    Update funzioni.js

--- funzioni.js
+++ funzioni.js
@@ -47,3 +47,42 @@ document.getElementById("registrationForm").addEventListener("submit", async (e)
         alert("Errore: " + error.message);
     }
 });
+
+
+
+/*document.getElementById("registrationForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    let username = document.getElementById("username").value;
+    let email = document.getElementById("email").value;
+
+    // Verificación de admin
+    admin(username, email);
+
+    // Guarda el usuario en localStorage
+    guardarUsuario(username, email);
+
+    // Muestra una alerta de registro exitoso
+    alert("¡Registro exitoso!");
+
+    // Restablece el formulario
+    document.getElementById("registrationForm").reset();
+});
+
+function admin(username, email) {
+    if (username === "admin" && email === "admin@es") {
+        window.location.href = 'admin.htm';  // Redirige a la página de admin
+    }
+}
+ 
+    console.log("Document written with ID: ", docRef.id); // Aggiungi questa linea per vedere se i dati vengono effettivamente registrati
+function guardarUsuario(username, email) {
+    // Recupera la lista de usuarios existente o crea un array vacío
+    let usuarios = JSON.parse(localStorage.getItem("users")) || [];
+
+    // Agrega el nuevo usuario al array
+    usuarios.push({ username: username, email: email });
+
+    // Guarda el array actualizado en localStorage
+    localStorage.setItem("users", JSON.stringify(usuarios));
+}*/
